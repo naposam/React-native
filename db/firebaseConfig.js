76a93128923e873c00977e348ce7820a81372791
kumi <49414569+naposam@users.noreply.github.com>
2019-12-09 12:56:57 +0000

    Update firebaseConfig.js

--- db/firebaseConfig.js
+++ db/firebaseConfig.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import firebase from 'firebase'
 const firebaseConfig = {
-  apiKey: "AIzaSyD2n_bG2eAtcvrzRrzEIHkntYENV1bQmVY",
-  authDomain: "fir-db-98433.firebaseapp.com",
-  databaseURL: "https://fir-db-98433.firebaseio.com",
-  projectId: "fir-db-98433",
-  storageBucket: "fir-db-98433.appspot.com",
-  messagingSenderId: "799215944568",
-  appId: "1:799215944568:web:a0fdcdae659eb5b98d7e8d",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
   measurementId: "G-V4JEL0H1JG"
 };
   if (!firebase.apps.length) {
